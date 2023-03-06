feb9c84e3abb8d6fee9c3be4703fbd18ec271624
YLi32498 <85614113+YLi32498@users.noreply.github.com>
2023-03-06 02:33:46 +0100

    Update main2D_norm_based_method.m

--- main2D_norm_based_method.m
+++ main2D_norm_based_method.m
@@ -8,8 +8,7 @@
 % initial state
 n = 2;
 
-% X0_1 = [0.9,1.1];
-% X0_2 = [-0.1,0.1];
+
 
 x0 = [10.5,-5.5]';
 xc = [10,-5]';
@@ -20,10 +19,10 @@ Tend = 20;
 step = 0.01;
 t = 0:step:Tend;
 
-U = 0.5;
+Ub = 0.5;    %% upperbound of input ub
 figure()
 hold on
-plot(t,U*sin(t))
+plot(t,Ub*sin(t))
 xlabel('time (s)')
 ylabel('input')
 % legend('u')
@@ -31,15 +30,16 @@ title('Input signal')
 hold off
 A = [-1 -4; 4, -1];
 B = [1;1]; 
-C = [1 0];
-sys = ss(A,B,C,0);
+E = [1;1];
+H = [1 0];
+sys = ss(A,B,H,0);
 
 
-% Ud = -0.3+0.6*rand(length(t),1);
-ud = 0.05;
-Ud = -ud+2*ud*rand(length(t),1);
+db = 0.05;                         %% upperbound of disturbance db
+Ud = -db+2*db*rand(length(t),1);  %% disturbance d
 
-Uu = U*sin(t)';
+
+Uu = Ub*sin(t)';            %% input u
 % plant trajectory
 
 tspan = [0 Tend];
@@ -59,13 +59,9 @@ title('Plant trajectories')
 hold off
 
 %% solve LMI for P and nu_1 nu_2
-iii=1;
-% for iter = 0:0.001:0.80
-% E= [1;1];
-% gamma_old = 0;
-tune = 7.90;
+
+tune = 7.90;            %tuninng LMI paramater for better local observer result
 tune2 = 20;
-% for iii = 1:100
 
 setlmis([]) 
 p = lmivar(1,[2 1]);
@@ -76,7 +72,7 @@ pk = lmivar(2,[2 1]);
 lmiterm([1 1 1 p],1,A,'s');   % A^TP+ PA
 
 
-lmiterm([1 1 1 pk],1,C,'s');   % A^TP+ PA
+lmiterm([1 1 1 pk],1,H,'s');   % A^TP+ PA
 
 
 lmiterm([1 1 1 nu1],1,eye(2));   
@@ -118,7 +114,7 @@ PK = dec2mat(lmis,xfeas,pk);
 K = inv(P)*PK;
 L = -K;
 
-A_bar = A + K*C;
+A_bar = A + K*H;
 
 % Nu1=100
 % Nu2 = 10000
@@ -141,7 +137,7 @@ Lam_min = E_p(1);
 Lam_e = Nu1./Lam_max/n;
 
 
-gamma_hat = sqrt(n*Nu2./Lam_min./Lam_e)*ud
+gamma_hat = sqrt(n*Nu2./Lam_min./Lam_e)*db  %% gamma_hat(db) function in the local observer design part
 
 
 % tune = tune + 70*(gamma_hat-gamma_old);
@@ -160,11 +156,11 @@ gamma_hat = sqrt(n*Nu2./Lam_min./Lam_e)*ud
 % assume model used for observer is identical to the plant
 Aobs = A;
 Bobs = B;
-Cobs = C;
+Cobs = H;
 % obtain both plant and observer in a single system called sysNew
 % first 2 state are plant states, last 2 states are observer states
 Anew = [A      zeros(2);
-        L*C    Aobs-L*Cobs]; % output of the plant affects the observer 
+        L*H    Aobs-L*Cobs]; % output of the plant affects the observer 
                              %    states due to the way observers work
 %first input to the system with observer is the unknown disturbance, 
 %   which only affects the plant not the observer, 
@@ -174,7 +170,7 @@ Bnew = [B          B
         zeros(2,1) Bobs]; 
 % first output is plant output,  
 %    last 2  are observer state estimates    
-Cnew = [C          zeros(1,2); 
+Cnew = [H          zeros(1,2); 
         zeros(2,2) eye(2)];
 sysNew = ss(Anew,Bnew,Cnew,0);
 
@@ -205,7 +201,7 @@ for i=1:length(xobs(:,1))
 %     beta_hat(i) = sqrt(2*Lam_max./Lam_min)*exp(-Lam_e*t(i)./2)*xb;    % 0.1 from x_b
 end
 
-beta_hat = @(t) sqrt(2*Lam_max./Lam_min)*exp(-Lam_e*t./2)*xb;
+beta_hat = @(t) sqrt(2*Lam_max./Lam_min)*exp(-Lam_e*t./2)*xb;  %% beta_hat function in the local observer design part
 
 
 figure()
@@ -224,17 +220,7 @@ ylabel('estimate error','FontSize',15)
  
 xobs = xobs';
 x_obs = xobs(3:4,:);
- %% test for quantization
 
-
- 
-
-% S = rand(1)*2-1
-% C = 0;
-% L = 1;
-% N  = 100;
-% Se  = Quanti(S,C,L,N,0)
-% Sd = Quanti(Se,C,L,N,1)
 
 %% Initial quantization parameters
 
@@ -244,7 +230,7 @@ L_0 = [1;1]*xb;
 
 N = 4; %Quantization level
 % N=20;
-Tq = 0.1; % time between each transmission 
+Tq = 0.1; % inter-transmission time T
 
 % built zonotope
 
@@ -256,20 +242,15 @@ G_Z{1} =eye(2)*xb;
 %% start simulating
 close all
 % Ck(:,1) = C_0;
-Cd(:,1) = C_0;  % docoded packet
+Pd(:,1) = C_0;  % docoded packet
 
 
-% for j = 1:n
-%     G_Zr{1} = reduceorder(G_Z{1},n);
-%     Lk1(j,1)= C_Z{1}(j) + G_Zr{1}(j,j);    % initial quantization region's element-wise upper bound  
-%     Lk2(j,1)= C_Z{1}(j) - G_Zr{1}(j,j);    % lower bound
-% end
 
 Cq(:,1) = C_0;
 Lk(:,1) = L_0;
 
-xk(:,1) = x0;       % plant state
-xs(:,1) = x0;      %actual state
+xk(:,1) = x0;      % plant state
+xs(:,1) = x0;      % actual state
 T_total = 0:Tq:Tend; 
 
 % store
@@ -283,7 +264,6 @@ xe = cell(0);
 xr = cell(0);
 xL = cell(0);
 Tr_plot = [];
-Xr_plot = [];
 
 
 figure()
@@ -295,137 +275,104 @@ title('Dynamic Quantization Scheme using Hypercubes','FontSize',15)
 Tsim = step;
 
 
-maxplot = 20;
+maxplot = 20; % maximum number of quantization scheme to be plotted
+
 for k = 1:length(T_total)-1
-% for k = 1:10
 
-
-%     [To;9_k{k},x{k}] = ode45(@(t,x) nlinearFode(t,x,A,H,n), [(k-1)*Tq k*Tq], [xk(:,k);Cd(:,k)]);
     
     T_k{k} = (k-1)*Tq:Tsim:k*Tq;
-    xp{k} = x_obs(:,round((k-1).*Tq./step)+1:round(k.*Tq./step));           %store plant trajc
-    xr{k} = lsim(sys,0*T_k{k},0:Tsim:Tq,Cd(:,k));                        % store reconstucted state
-%     xp{k} = xp{k}';
-    xr{k} = xr{k}';
+    xp{k} = x_obs(:,round((k-1).*Tq./step)+1:round(k.*Tq./step));           %store local estimated state's trajc
     
 %  plot over-approximated quantization region
     if k<maxplot
        p(1) = plot([xp{k}(1,:), x_obs(1,round(k.*Tq./step)+1)] ,[xp{k}(2,:) x_obs(2,round(k.*Tq./step)+1)],'-g');
        p(2) =  scatter(xp{k}(1,1),xp{k}(2,1),40,[0 0 0],'filled');
 
-        G_Z{k} = [Lk(1,k) 0; 0 Lk(2,k)];
-        C_Z{k} = Cq(:,k);
-        ppppp=PlotZonotope(G_Z{k},C_Z{k});
+        G_Z{k} = [Lk(1,k) 0; 0 Lk(2,k)]; %% zonotope generator matrix [g1 g2 ...]
+        C_Z{k} = Cq(:,k);               %%zonotope center vector 
+        plotz=PlotZonotope(G_Z{k},C_Z{k});  %plot zonotopes
     end
 
 
     % update state for next ode's initial plant state
      xk(:,k+1) =  xp{k}(:,end);  
      xs(:,k+1) =  xp{k}(:,1); 
-    
-%     Lk1(:,k+1) = 1./N*(exp(Lx*Tq).*Lk1(:,k)) + xL{k}(1:n,end);
-% 
-%     Lk2(:,k+1) = 1./N*(exp(Lx*Tq).*Lk2(:,k)) + xL{k}(n+1:2*n,end);
+
         
     
     %begin quantization
     
-    Se = Quanti(xp{k}(:,1),Cq(:,k),Lk(:,k),N,0);
-    Sd = Quanti(Se,Cq(:,k),Lk(:,k),N,1);
-    Sd = Sd';
+    Pte = Quanti(xp{k}(:,1),Cq(:,k),Lk(:,k),N,0);  %% encoded packet Pe at k-th transmission 
+    Ptd = Quanti(Pte,Cq(:,k),Lk(:,k),N,1);          %% decoded packet Pd at k-th transmission
+    Ptd = Ptd';
     % Update decoded packet for updating the next quantization centroid
-    Cd(:,k+1) =  Sd;
+    Pd(:,k+1) =  Ptd;           %% store the decoded packet Pd
     
 %     check for overflow
-        Se = Se';
-    if max(Se) >= N
+        Pte = Pte';
+    if max(Pte) >= N
         warning('overflooting') 
     end
-    if min(Se) < 0
+    if min(Pte) < 0
         warning('overflooting')
    
     end
     
 %     store overflow index
-    overflow(:,k) = Se ;
+    overflow(:,k) = Pte ;
 
 %         plot quantization subregion and decoded state
 
     if k==1||k == 2||k==4||k==6||k==8
         [x,y] = meshgrid(C_Z{k}(1)-G_Z{k}(1,1)+ 2*(0:N).*(G_Z{k}(1,1))./N, C_Z{k}(2)-G_Z{k}(2,2)+ 2*(0:N).*(G_Z{k}(2,2))./N);
-        pp = plot(x(1),y(1),'--','Color','r');
+        plotsubregion = plot(x(1),y(1),'--','Color','r');
         plot(x,y,'--','Color','r')
 plot(x',y','--','Color','r')
 
     end
     
      if k<maxplot
-  ppp = scatter(Cd(1,k+1),Cd(2,k+1),40,[0 0 1],'filled');  
+  plotdecodedpacket = scatter(Pd(1,k+1),Pd(2,k+1),40,[0 0 1],'filled');  
      end
 
     % calculate the beta functions
-    Beta = (exp(Tq*norm(A,'inf'))-1)./norm(A,'inf').*(U);
-    Betad = beta_hat(k*Tq)+gamma_hat;
+    Beta = (exp(Tq*norm(A,'inf'))-1)./norm(A,'inf').*(Ub); % function beta  
+    Betad = beta_hat(k*Tq)+gamma_hat;                      % function beta_d
     
-    Betae = (exp(Tq*norm(A,'inf'))-1)./norm(A,'inf').*(norm(K*C,'inf')*Betad);
+    Betae = (exp(Tq*norm(A,'inf'))-1)./norm(A,'inf').*(norm(K*H,'inf')*Betad);  %function beta_e
     Lam = expm(A.*Tq);
     LamHAT = abs(Lam);
-    
-    
-     % Zk+1 = Lam*Zk + Z_beta
-    
- 
-     
-
-      
-     
+  
      
      %overapproximating the terminal reachable set using zonotope
     % and update quantization parameters
     
-
-    
-    Cq(:,k+1) = Lam*Cd(:,k+1);
+    % quantization update law for norm-based method
+    Cq(:,k+1) = Lam*Pd(:,k+1);      
     for i = 1:n
         Lk(i,k+1) = Beta+Betae+ exp(norm(A,'inf').*Tq)*Lk(i,k)./N;
     end
         
     
-    %      plot the terminal reachable set zonotope
-    if k<maxplot-1
-        Czz = Lam*Sd;
-%         Gzz = Lam/N*G_Z{k};
-        Gzz = [(Beta+Betae+ exp(norm(A,'inf').*Tq)*Lk(i,k)./N)*eye(2)];
-%         pppppp=PlotZonotope2(Gzz,Czz);
-    end
-
-      
-    
+%     %      plot the terminal reachable set zonotope
+%     if k<maxplot-1
+%         Czz = Lam*Ptd;
+% 
+%         Gzz = [(Beta+Betae+ exp(norm(A,'inf').*Tq)*Lk(i,k)./N)*eye(2)];
+% 
+%     end
 
 
-
-    
-%     G_Z{i} = reduceorder(G_Z{i},8);
 
     %store reconstruced state
-    
-%     plot(T_k{k},xr{k},'Color','red')
-    Tr_plot  = [Tr_plot,T_k{k}];
-    Xr_plot = [Xr_plot,xr{k}];
-end
-% txt = 'k = 1';
-% text(1,1.75,txt,'FontSize',15)
-% 
-% txt = 'k = 3';
-% text(0.2-0.05,1,txt,'FontSize',15)
-% 
-% txt = 'k = 4';
-% text(-0.1,0.9 ,txt,'FontSize',15)
-% 
-% txt = 'k = 5';
-% text(-0.3,0.8 ,txt,'FontSize',15)
 
-legend([p(1) p(2) ppppp pp ppp],'local state estimate $\hat{x}(t)$','local state estimate at the transmission time $\hat{x}(t_k)$','quantization region $S_Q^k$','quantization subregions $S_q^k$','decoded state $P_d^k$','FontSize',15,'Interpreter','latex')
+    Tr_plot  = [Tr_plot,T_k{k}];
+
+end
+
+
+legend([p(1) p(2) plotz plotsubregion plotdecodedpacket],'local state estimate $\hat{x}(t)$','local state estimate at the transmission time $\hat{x}(t_k)$','quantization region $S_Q^k$','quantization subregions $S_q^k$','decoded state $P_d^k$','FontSize',15,'Interpreter','latex')
+
 %%
 % plot quantization level
 figure()
@@ -441,9 +388,9 @@ for i = 1:ne+1
     for j =0:N
        line( [0.1*(i-1)-0.01 ,0.1*(i-1)+0.01], [ Cq(1,i)-Lk(1,i)+2*j/N*Lk(1,i) Cq(1,i)-Lk(1,i)+2*j/N*Lk(1,i)]   ,'Color','red'    )
     end
-    p(3) = scatter(0.1*(i-2),Cd(1,i),10,[0 0 1],'filled');
+    p(3) = scatter(0.1*(i-2),Pd(1,i),10,[0 0 1],'filled');
 end
-scatter(0.1*(i-1),Cd(1,i+1),10,[0 0 1],'filled')
+scatter(0.1*(i-1),Pd(1,i+1),10,[0 0 1],'filled')
 
 legend([p(1) p(2) p(3)],'local state estiamte $\hat{x}_1$','quantization range $[C_1-L_1,C_1+L_1]$','deocoded state $P_{d,1}$','FontSize',11,'Interpreter','latex')
 
@@ -463,9 +410,9 @@ for i = 1:ne+1
         for j =0:N
         line( [0.1*(i-1)-0.01 ,0.1*(i-1)+0.01], [ Cq(2,i)-Lk(2,i)+2*j/N*Lk(2,i) Cq(2,i)-Lk(2,i)+2*j/N*Lk(2,i)]   ,'Color','red'    )
         end
-   p(3)=  scatter(0.1*(i-2),Cd(2,i),10,[0 0 1],'filled');
+   p(3)=  scatter(0.1*(i-2),Pd(2,i),10,[0 0 1],'filled');
 end
-scatter(0.1*(i-1),Cd(2,i+1),10,[0 0 1],'filled')
+scatter(0.1*(i-1),Pd(2,i+1),10,[0 0 1],'filled')
 legend([p(1) p(2) p(3)],'local state estimate $\hat{x}_2$','quantization range $[C_2-L_2,C_2+L_2]$','deocoded state $P_{d,2}$','FontSize',15,'Interpreter','latex')
 xlim([0.07,1.01])
 hold off
@@ -473,7 +420,7 @@ hold off
 
 %%
 err_max = Lk/N;
-err_q = abs(xs-Cd);
+err_q = abs(xs-Pd);
 L_q_sim = err_max(:,end);
 % L_q_sim = vpa(L_q_sim,3);
 figure()
@@ -500,9 +447,3 @@ legend('$\bar{e}_{q,2}$','$e_{q,2}$','Interpreter','latex','FontSize',20)
 hold off
 text(max(xlim)-20, 0.1, sprintf('$L_{q,2}= 0.0684 $'), 'Horiz','left', 'Vert','bottom','FontSize',20,'Interpreter','latex')
 
-
-
-
-
-%% testing for error convergance
-(eye(n)-LamHAT./N)^(-1)*[1;1]*Beta /4
